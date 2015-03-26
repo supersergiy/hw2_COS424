f26e377256851c8f048bbbb0ac509feb70b2cc0f
Sergiy Popovych <popovych@davisson.princeton.edu>
2015-03-26 11:15:05 -0400

    tabulation

--- parser.py
+++ parser.py
@@ -17,12 +17,12 @@ try:
 except getopt.GetoptError:
   print 'error'
   sys.exit(2)
-  
+
 for opt, arg in opts:
   if opt in ("-n", "--num"):
-num_interval = arg
+    num_interval = arg
   elif opt in ("-f", "--file"):
-train_file = arg
+    train_file = arg
   elif opt in ("-i","--intsize"):
     interval_size = arg
 # first, collect the features
@@ -38,51 +38,52 @@ for line in raw_data:
   start_loc = int(row[1])
   data[start_loc] = np.loadtxt(row[4:-1])
   label[start_loc] = int(row[-1])
+
 raw_data.close()
 
 print 'buidling features'
 
 features = {}
 for i in data:
-  features[i] = data[i]
-  dat_size = data[i].shape
-  
-  for j in range(1,(int(num_interval)-1)/2+1):
-    start_loc = i + (j-1)*int(interval_size)
+   features[i] = data[i]
+   dat_size = data[i].shape
+
+   for j in range(1,(int(num_interval)-1)/2+1):
+      start_loc = i + (j-1)*int(interval_size)
     # pos offset
-    end_int = i + j*int(interval_size)
-    sum = np.zeros(dat_size)
-    count = np.zeros(dat_size)
-    k = start_loc + 2
-    while(k <= end_int):
- if k in data:
-   checknan = ~np.array(np.isnan(data[k])) #skip nan, so need to keep count correctly
-   data[k][np.isnan(data[k])] = 0 #change nan to zero to skip it in the average
-   sum += data[k]
-   count += checknan
- k += 2
-    average = np.zeros(dat_size)
-    count[count == 0] = 1 #clear the case where count is zero
-    average = sum/count
-    features[i] = np.concatenate((features[i],average),axis=0)
-    # neg offset
-    end_int = i - j*int(interval_size)
-    sum = np.zeros(dat_size)
-    count = np.zeros(dat_size)
-    start_loc = i - (j-1)*int(interval_size)
-    k = start_loc - 2
-  
-    while(k >= end_int):
- if k in data:
-   checknan = ~np.array(np.isnan(data[k])) #skip nan, so need to keep count correctly
-   data[k][np.isnan(data[k])] = 0 #change nan to zero to skip it in the average
-   sum += data[k]
-   count += checknan
- k -= 2
-    average = np.zeros(dat_size)
-    count[count == 0] = 1 #clear the case where count is zero 
-    average = sum/count
-    features[i] = np.concatenate((features[i],average),axis=0)
+      end_int = i + j*int(interval_size)
+      sum = np.zeros(dat_size)
+      count = np.zeros(dat_size)
+      k = start_loc + 2
+      while(k <= end_int):
+         if k in data:
+            checknan = ~np.array(np.isnan(data[k])) #skip nan, so need to keep count correctly
+            data[k][np.isnan(data[k])] = 0 #change nan to zero to skip it in the average
+            sum += data[k]
+            count += checknan
+         k += 2
+         average = np.zeros(dat_size)
+         count[count == 0] = 1 #clear the case where count is zero
+         average = sum/count
+         features[i] = np.concatenate((features[i],average),axis=0)
+   # neg offset
+   end_int = i - j*int(interval_size)
+   sum = np.zeros(dat_size)
+   count = np.zeros(dat_size)
+   start_loc = i - (j-1)*int(interval_size)
+   k = start_loc - 2
+
+   while(k >= end_int):
+      if k in data:
+         checknan = ~np.array(np.isnan(data[k])) #skip nan, so need to keep count correctly
+         data[k][np.isnan(data[k])] = 0 #change nan to zero to skip it in the average
+         sum += data[k]
+         count += checknan
+      k -= 2
+      average = np.zeros(dat_size)
+      count[count == 0] = 1 #clear the case where count is zero
+      average = sum/count
+      features[i] = np.concatenate((features[i],average),axis=0)
 # now, collect the teacher
 print 'reading sample/test data'
 
@@ -91,21 +92,21 @@ raw_data = open(inputfile)
 
 teacher = {}
 for line in raw_data:
-  row = line.split()
-  if(int(row[-1]) == 1):
-    loc = int(row[1])
-    teacher[loc] = float(row[4])
+   row = line.split()
+   if(int(row[-1]) == 1):
+      loc = int(row[1])
+      teacher[loc] = float(row[4])
 raw_data.close()
 inputfile = proj_path + '/' + data_path + '/' + test_file
 raw_data = open(inputfile)
 for line in raw_data:
-  row = line.split()
-  if(int(row[-1]) == 0):
-    loc = int(row[1])
-    teacher[loc] = float(row[4])
+   row = line.split()
+   if(int(row[-1]) == 0):
+      loc = int(row[1])
+      teacher[loc] = float(row[4])
 raw_data.close()
 
-# write to files 
+# write to files
 print 'writing to files'
 
 output_Xtrain = './cos424hw2_Xtrain_' + num_interval
@@ -123,25 +124,25 @@ outLtrain = open(output_Ltrain,'w')
 outLtest = open(output_Ltest,'w')
 
 for i in features:
-  
-  if(label[i] == 0):
+
+   if(label[i] == 0):
     # test data
-np.savetxt(outXtest,features[i],newline=' ')
-outXtest.write("\n")
-outYtest.write(str(teacher[i]))
-outYtest.write("\n")
-outLtest.write(str(i))
-outLtest.write("\n")
-  elif(label[i] == 1):
+      np.savetxt(outXtest,features[i],newline=' ')
+      outXtest.write("\n")
+      outYtest.write(str(teacher[i]))
+      outYtest.write("\n")
+      outLtest.write(str(i))
+      outLtest.write("\n")
+   elif(label[i] == 1):
     #train data
-np.savetxt(outXtrain,features[i],newline=' ')
-outXtrain.write("\n")
-outYtrain.write(str(teacher[i]))
-outYtrain.write("\n")
-outLtrain.write(str(i))
-outLtrain.write("\n")
-  else:
-    print "something odd just happened."
+      np.savetxt(outXtrain,features[i],newline=' ')
+      outXtrain.write("\n")
+      outYtrain.write(str(teacher[i]))
+      outYtrain.write("\n")
+      outLtrain.write(str(i))
+      outLtrain.write("\n")
+   else:
+      print "something odd just happened."
 outXtest.close()
 outYtest.close()
 outXtrain.close()
